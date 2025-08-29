c308f841fcc7267b91dd5d289a9d1c8b76b7c566
Muditha <mudithcse@gmail.com>
2025-08-29 17:49:37 +0530

    default update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Aura_login from './Aura_login/Aura_login';
-import Aura_sign from './Aura_signin/Aura_sign'; // your register page
+import Aura_sign from './Aura_signin/Aura_sign';
 
 function App() {
   return (
-    <BrowserRouter basename="/React_1st">
-      <Router>
-        <Routes>
-          {/* Login page */}
-          <Route path="/" element={<Aura_login />} />
-
-          {/* Register page */}
-          <Route path="/register" element={<Aura_sign />} />
-        </Routes>
-      </Router>
-    </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        {/* Login page */}
+        <Route path="/" element={<Aura_login />} />
+        {/* Register page */}
+        <Route path="/register" element={<Aura_sign />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
