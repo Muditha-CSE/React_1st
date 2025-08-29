b59cbc6fad69d4eb5fe941a07970de7a2bb5119f
Muditha <mudithcse@gmail.com>
2025-08-29 17:07:36 +0530

    default update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/React_1st/',   // ← your GitHub repo name here
   plugins: [react()],
-});
+  base: '/React_1st/', // Replace with your actual repo name
+})
